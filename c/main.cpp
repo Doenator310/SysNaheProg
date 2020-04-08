701ee05462d573193cd81db567c06a466dfb77b8
Doenator <David@dieterhenkel.de>
2020-04-08 16:31:48 +0200

    ausversehen gelöschtes wieder hinzugefügt

--- c/main.cpp
+++ c/main.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
-#include <QDebug>
-//stack
+
+int getStringLength(const char* txt){
+    int size = 0;
+    for(;txt[size];size++);
+    return size;
+}
+
+void copyString(const char* src, char* dest){
+    do{
+        *dest++ = *src++;
+    }
+    // - 1 wegen nullbyte
+    while (*(src-1)!=NULL);
+}
+
 int summe(int anzahl, int summand1, ...){
     int* werte = &summand1;
     int summe = 0;
     for(int i = 0; i < anzahl; i++){
-        qDebug() << werte[i];
+        std::cout << werte[i] << std::endl;
         summe += werte[anzahl - i -1];
     }
     return summe;
@@ -20,6 +33,11 @@ void ausgabe(const char* text, ...){
 
 int main(int argc, char *argv[], char** env)
 {
+    const char* txt = "YEET";
+    char txt2[10];
+    std::cout << getStringLength(txt) << std::endl;
+    copyString(txt,txt2);
+    std::cout << txt2 << std::endl;
     ausgabe("EINS","ZWEI","DREI", NULL);
     return 0;
 }
